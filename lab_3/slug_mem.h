393b61c3a545fdcf8f1b9b6283b599a0c62fa5ec
Falcon Wong <wywfalcon@aim.com>
2014-05-12 16:55:47 -0700

    First try
    Implemented both free and alloc

--- lab_3/slug_mem.h
+++ lab_3/slug_mem.h
@@ -1,6 +1,7 @@
 // CREATED
 
 #include <unistd.h>
+#include <time.h>
 
 #define malloc(s) slug_malloc((s))
 #define FUNCTIONIZE(a,b) a(b)
@@ -9,21 +10,26 @@
 #define FILE_POS __FILE__ ":" INT2STRING(__LINE__)
 
 #define MAX_ALLOC_SIZE 128
+#define FALSE 0
+#define TRUE 1
 
 typedef struct *node Node;
 
-typedef struct node {
+typedef struct Node {
     int block_size;
-	char alloc_state;
-	Node next_node;
-} node;
+	void *address;
+	char *caller;
+	time_t time_stamp;
+	Node next;
+	Node prev;
+} Node;
 
-typedef struct mem {
+typedef struct Mem {
     Node first;
 	Node last;
 	Node current;
 	int mem_size;
-} mem;
+} Mem;
 
 void *slug_malloc (size_t size, char *WHERE);
 void slug_free ( void *addr, char *WHERE );
