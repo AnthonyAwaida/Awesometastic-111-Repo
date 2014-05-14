0c7428eb76682a813203dd817b86b6f12fe58a20
JeffreyKDeng <jkdeng@ucsc.edu>
2014-05-13 20:19:14 -0700

    Update slug_mem.c
    Wrote memstats and did some comments on what I added/ possible errors that I found

--- lab_3/slug_mem.c
+++ lab_3/slug_mem.c
@@ -3,24 +3,31 @@
 Mem memory;
 
 void mem_init (void) {
-    memory = malloc(MAX_ALLOC_SIZE);
+        memory = malloc(MAX_ALLOC_SIZE);
 	memory->first = NULL;
 	memory->last = NULL;
 	memory->current = NULL;
 	memory->mem_size = MAX_ALLOC_SIZE;
+	memory->num_alloca = 0;   //added this for counting total # of allocations done overall.
+	memory->total_size = 0;
+	memory->mean = 0;
+	memory->SD = 0;
 }
 
 void insert_node (Node node) {
     if (memory->first == NULL) {
 	    memory->first = memory->last = node;
-		memory->first->prev = memory->first->next = NULL;
+	    memory->first->prev = memory->first->next = NULL;
 	} else {
 		node->prev = memory->last;
 		memory->last->next = node;
-	    memory->last = node;
+	        memory->last = node;
 	}
 	memory->current = node;
 	node->next = NULL;
+	memory->num_alloca++;  //increase by 1 for each new allocation
+	memory->total_size+=node->block_size;
+	memory->mean = memory->total_size / memory->num_alloca;
 }
 
 int mem_is_valid (void *address, char *caller) {
@@ -29,6 +36,8 @@ int mem_is_valid (void *address, char *caller) {
 	    if (memory->current->address == address && memory->current->caller == caller) {
 		    return TRUE;
 		}
+		//doesn't the while loop needs to change the memory->current so it'll loop?  should add:
+	          memory->current = memory->current->next;
 	}
 	return FALSE;
 }
@@ -55,7 +64,7 @@ void *slug_malloc (size_t size, char *WHERE) {
 	    return;
     }
 	/* Saves address and size */
-    new_node->address = data_address;
+        new_node->address = data_address;
 	new_node->block_size = size;
 	
 	/* Create time stamp */
@@ -65,8 +74,9 @@ void *slug_malloc (size_t size, char *WHERE) {
 	/* Saves caller info */
 	new_node->caller = WHERE;
 	
-    /* Insert node */
-	insert_node(memory, new_node);
+        /* Insert node */
+	insert_node(memory, new_node);  //did you called insert_node wrong?  it takes in a single node parameter 
+	                                // shouldn't it be: insert_node(new_node); ?
 }
 
 void slug_free ( void *addr, char *WHERE ) {
@@ -81,5 +91,25 @@ void slug_free ( void *addr, char *WHERE ) {
 }
 
 void slug_memstats ( void ) {
+	int Num_active_alloc=1;
+	int total_active_size=0;
+        
+	Node node = malloc(sizeof(Node));
+	node = memory->first;
+	while(node !=NULL){
 
+		printf("allocation[%d] %s(%11d) - size: %d, address: % p \n", 
+		        Num_active_alloca, node->caller, node->time_stamp, node->block_size, node->address);
+		Num_active_alloca++;
+		total_active_size += node->block_size;
+		node = node->next;
+	}
+
+	printf("----------------------------------------------------------\n");
+	printf("*****               Allocation Summary               *****\n");
+	printf("Total number of allocations done: %d\n", memory->num_alloca);
+	printf("Total number of active allocation: %d\n", Num_active_alloca);
+	printf("Total memory size of active allocation: %d\n", total_active_size);
+	printf("Mean: %f   SD: %f\n",memory->mean, memory->SD);
+	printf("----------------------------------------------------------\n");
 }
