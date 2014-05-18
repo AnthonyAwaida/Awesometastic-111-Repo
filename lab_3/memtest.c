ee3f2e1f1ddc3b1d66bb1726d2d1ddb6023a389d
Falcon Wong <wywfalcon@aim.com>
2014-05-18 13:45:01 -0700

    With all compiling correctly

--- lab_3/memtest.c
+++ lab_3/memtest.c
@@ -2,7 +2,6 @@
 #include <stdio.h> 
 #include "slug_mem.h"
 
-
 /* needs to test the following functionality:
 	* regular allocation & freeing
 	* allocating size 0
@@ -11,17 +10,17 @@
 */
 int main(void){
 
-    char caller = "memtest:main";
+    char *caller = "memtest:main";
 	/* regular allocation, no errors expected */
-	int *test1 = malloc(sizeof(int), caller);
+	int *test1 = malloc(sizeof(int));
 
 	/* allocating zero space, expect message to stderr */
 	size_t empty = (size_t) 0;
-	void *test2 = malloc(empty, caller);
+	void *test2 = malloc(empty);
 
 	/* allocating >= 128mb, should print to stderr and end */
 	size_t too_much = MAX_ALLOC_SIZE;
-	void *test3 = malloc(too_much, caller);
+	void *test3 = malloc(too_much);
 
 	/* shouldn't be printed */
 	printf("don't print me\n");
