b9fb9d35e6e1ecf9ffe4d5b4197dbbc870af1d4b
xtang2 <xiaolilillytang@dhcp-203-180.cruznet.ucsc.edu>
2014-05-18 16:45:22 -0700

    Memtest2 corrected

--- lab_3/memtest2.c
+++ lab_3/memtest2.c
@@ -19,21 +19,21 @@ int main(void){
 	/* trying to deallocate an invalid address*/
 	printf("***** Test for invalid address  *****\n");
 	int *test1 = (int *)malloc(sizeof(int)*random);
-	free(test1, "memtest.c:21");
+	free(test1);
 	free(invalid);
 	
 	/*trying to deallocate an already freed region*/
 	printf("***** Test for already freed region  *****\n");
 	int *test2 = (int *)malloc(sizeof(int)*random);
-	free(test2, "memtest.c:28");
-	free(test2, "memtest.c:28");
+	free(test2);
+	free(test2);
 	
 	/*trying to deallocate a valid region by passing in a pointer inside the region*/
 	printf("***** Test for valid region  *****\n");
 	int *test3 = (int *)malloc(sizeof(int)*random);
 	int *test4 = (int *)malloc(sizeof(int)*random);
-	free(test3, "memtest.c:35");
-	free(test4, "memtest.c:36");
+	free(test3);
+	free(test4);
 	
 	return 0;
 }
